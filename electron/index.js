a74091d7fc0ea255080e1d33538b94e49712ca60
Cristian Curtinaz <curtinazif@gmail.com>
2023-10-04 21:02:53 -0300

    Electron added

---
+++ electron/index.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false, // Isso é importante para manter a segurança.
+      preload: __dirname + '/preload.js', // Opcional, para injetar scripts na WebView.
+    },
+  });
+
+  win.loadFile('../index.html'); // Carrega o arquivo HTML na WebView.
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
